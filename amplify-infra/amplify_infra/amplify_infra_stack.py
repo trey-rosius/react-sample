734b7437a50874ebdf85459edacdda4c6e30c04c
iMAC <treyrosius@gmail.com>
2021-05-22 17:46:41 +0100

    test

--- amplify-infra/amplify_infra/amplify_infra_stack.py
+++ amplify-infra/amplify_infra/amplify_infra_stack.py
@@ -1,6 +1,8 @@
 from aws_cdk import core as cdk
+
 import aws_cdk.aws_codebuild as codebuild
 import aws_cdk.aws_amplify as amplify
+
 
 # For consistency with other languages, `cdk` is the preferred import name for
 # the CDK's core module.  The following line also imports it as `core` for use
@@ -13,5 +15,35 @@ class AmplifyInfraStack(cdk.Stack):
 
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+        amplify_app = amplify.App(self, "sample-react-app",
+           source_code_provider=amplify.GitHubSourceCodeProvider(
+        owner="trey-rosius",
+        repository="react-sample",
+        oauth_token=cdk.SecretValue.secrets_manager("trainer-github-token")),
+
+        build_spec=codebuild.BuildSpec.from_object_to_yaml({# Alternatively add a `amplify.yml` to the repo
+        "version": "1.0",
+        "frontend": {
+            "phases": {
+                "pre_build": {
+                    "commands": ["yarn"
+                    ]
+                },
+                "build": {
+                    "commands": ["yarn build"
+                    ]
+                }
+            },
+            "artifacts": {
+                "base_directory": "public",
+                "files": "**/*"
+            }
+            }})
+            )
+        amplify_app.add_branch("master")    
+
+
+  
 
         # The code that defines your stack goes here
